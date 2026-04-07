8a443130cd66656cae5cf4bcd5d143f79e0508b7
erikagiella <erikagiella93@gmail.com>
2026-04-07 19:58:51 +0200

    sistemata struttura form e aggiunti commenti

--- assets/functions.js
+++ assets/functions.js
@@ -1,15 +1,17 @@
-//funzione che riceve 4 variabili e ritorna una stringa contenente una card riempita col contenuto delle variabili
-function createCardText(name, role, email, img) {
+//funzione che riceve un oggetto member e restituisce una card 
+function createMemberCard(member) {
     //creo la stringa contenente la card 
     const cardHTML = `
     <div class="card border-0">
-      <img class="card-img rounded-circle" src="./assets/${img}" alt="">
-      <div class="card-body text-center">
-        <div class="memberName">${name}</div>
-        <div class="memberRole">${role}</div>
-        <div class="memberEmail">${email}</div>
+      <div class="card-img text-center">
+        <img class="w-50 rounded-circle" src="./assets/${member.img}" alt="">
       </div>
-    </div>  
+    <div class="card-body text-center">
+        <div class="memberName">${member.name}</div>
+        <div class="memberRole">${member.role}</div>
+        <div class="memberEmail">${member.email}</div>
+      </div>
+    </div>   
   `
     //ritorno la stringa card
     return cardHTML;
--- assets/script.js
+++ assets/script.js
@@ -37,33 +37,33 @@ const teamMembers = [
   }
 ];
 
-
+//OUR TEAM
+//inserimento nel DOM dei membri contenuti nell'array teamMembers
 //seleziono il nodo DOM 
 const membersListEl = document.getElementById("membersList")
-
-
 
 //ciclo gli elementi dell'array con un ciclo for
 for (let i = 0; i < teamMembers.length; i++) {
   //seleziono l'elemento dell'array utilizzando i come indice
   const member = teamMembers[i];
-  //creo le variabili a cui attribuire il valore delle proprietà dell'oggetto membro
-  const memberName = member.name;
-  const memberRole = member.role;
-  const memberEmail = member.email;
-  const memberImg = member.img;
   //creo un nuovo elemento div usando le API del DOM
   const colEl = document.createElement("div")
-
   //aggiungo al div le classi col
-  colEl.classList.add("col-12", "col-md-6", "col-lg-4");
-
+  colEl.classList.add("col-12", "col-md-6", "col-lg-4", "col-xl-3");
   //creo l'elemento card utilizzando l'apposita funzione
-  const cardHTML = createCardText(memberName, memberRole, memberEmail, memberImg);
-
+  const cardHTML = createMemberCard(member);
   //inserisco il testo nell'elemento col
   colEl.innerHTML = cardHTML;
-
   //aggiungo l'elemento col come nodo del DOM alla lista (nodo row)
   membersListEl.appendChild(colEl);
 }
+
+//           === FORM ===
+// inserisco un eventListener che al submit del form prenda
+// il contenuto nei campi del form e li inserisca nell'array teamMembers
+
+// seleziono il submit button del form
+
+// === EVENT LISTENER FORM SUBMIT ===
+
+// === RECUPERO INPUT DAL DOM ===
